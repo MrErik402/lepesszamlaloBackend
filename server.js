a69292b445eb27b25bf066492c16db00550febd1
MrErik402 <bujakierikattila@turr.hu>
2025-09-16 12:30:23 +0200

    Statisztika

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ loadUsers()
 loadSteps()
 // ENDPOINTS . USER
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Backend API by Bajai SZC Türr István Technikum - 13.A Szoftverfejlesztő')
 })
 
 //Get all user 
 
-app.get('/users', (req, res) => {
+app.get('/users', (_req, res) => {
   res.send(users)
 })
 
@@ -40,12 +40,12 @@ app.get('/users/:id', (req, res) => {
   if (idx > -1) {
     return res.send(users[idx])
   }
-  return res.status(400).send({msg: 'Nincs ilyen felhasználó'})
+  return res.status(400).send({message: 'Nincs ilyen felhasználó'})
 })
 
 //Get 404 oldal   
 
-app.get('/404', (req, res) => {
+app.get('/404', (_req, res) => {
   res.status(400).send("Itt bizony egy nagyon csúnya hibuska van aranyapóm")
 })
 
@@ -54,12 +54,12 @@ app.get('/404', (req, res) => {
 app.post('/users', (req, res) => {
   let data = req.body;
   if(isEmailExists(data.email)){
-    return res.status(400).send({msg: 'Ez az e-mail cím már regisztrált!'})
+    return res.status(400).send({message: 'Ez az e-mail cím már regisztrált!'})
   }
   data.id = getNextID()
   users.push(data)
   saveUsers()
-  res.send({msg: 'A felhasználó sikeresen regisztált!'})
+  res.send({message: 'A felhasználó sikeresen regisztált!'})
 });
 
 
@@ -88,9 +88,9 @@ app.delete('/users/:id', (req, res) => {
   if (idx > -1) {
     users.splice(idx, 1);
     saveUsers()
-    return res.send({msg: 'Felhasználó törölve'});
+    return res.send({message: 'Felhasználó törölve'});
   }
-  return res.status(400).send({msg: 'Nincs ilyen azonosítójű felhasználó'})
+  return res.status(400).send({message: 'Nincs ilyen azonosítójű felhasználó'})
 });
 // Felhasználói adatok módosítása 
 
@@ -163,92 +163,99 @@ app.patch('/users/:id', (req, res) => {
 
 // -----------------------------ENDPOINT - STEP---------------------------------
 
-// GET all steps by userID
+// GET all steps (all users)
+app.get('/steps', (req, res)=> {
+  res.send(steps);
+});
 
-app.get("/steps/user/:userID", (req,res) =>{
-  let userStep = []
-  let data = req.params;
-  let id = req.params.userID; //Itt a hátsó .id-nek a neve meg kell egyezzen a getbe lévő :után
-  let idx = steps.findIndex(step => step.userID == id); 
-  if (idx > -1) {
-    for (let i = 0; i < steps.length; i++) {
-      if(steps[i].userID == data.userID){
-        userStep.push(steps[i])
-      }
-    }
-    res.send(userStep)
-   
-  }
-  return res.status(400).send({msg: 'Nincs ilyen felhasználó'})
-})
+// GET all steps by userId
+app.get('/steps/user/:uid', (req, res)=> {
+  let userId = req.params.uid;
+  let idx = users.findIndex(user => user.id == userId);
 
-// GET one step by stepID
-app.get("/steps/:id", (req,res)=>{
-  let data = req.params;
-  let id = req.params.id; //Itt a hátsó .id-nek a neve meg kell egyezzen a getbe lévő :után
-  let idx = steps.findIndex(step => step.id == id); 
-  if (idx > -1) {
-    res.send(steps[idx])
+  if (idx == -1){
+      res.status(400).send({message: 'Nincs ilyen felhasználó!'});
+      return
   }
-  return res.status(400).send({msg: 'Nincs ilyen lépés'})
-})
+
+  res.send(steps.filter(step => step.userId == userId));
+});
+
+// GET one step by id
+app.get('/steps/:id', (req, res) => {
+  let id = req.params.id;
+
+  let idx = steps.findIndex(step => step.id == id);
+
+  if (idx == -1){
+      res.status(400).send({message: 'Nincs ilyen lépésadat!'});
+      return
+  }
+
+  res.send(steps[idx]);
+});
+
 // POST new step
-app.post("/steps/user/:userID", (req,res)=>{
+app.post('/steps', (req, res) => {
   let data = req.body;
-  data.id = getNextID("steps")
-  steps.push(data)
-  saveSteps()
-  res.send({msg: 'A lépés sikeresen hozzáadva'})
-})
-// PATCH step by stepID
-app.patch("/steps/:id", (req,res)=>{
-  const {id, date, count } = req.body;
-  let idx = steps.findIndex(step => step.id == id)
-  if(idx==-1){
-    return res.status(400).send({message: "Nem található a lépés"})
-  }
-  steps[idx].date = date
-  steps[idx].count = count
-
+  data.id = getNextID(steps);
+  steps.push(data);
   saveSteps();
+  res.send({message: 'A lépésszám felvéve!'});
+});
 
-  return res.status(200).send({message: "A lépés sikeresen módosítva!"});
-})
-// DELETE step by stepID
-app.delete("/steps/:id", (req,res)=>{
+// PATCH step by id
+app.patch('/steps/:id', (req, res)=>{
+  let id = req.params.id;
+  let data = req.body;
+
+  let idx = steps.findIndex(step => step.id == id);
+
+  if (idx > -1) {
+      steps[idx] = data;
+      steps[idx].id = Number(id);
+      saveSteps();
+      return res.send({message: 'A lépésadat sikeresen módosítva'});
+  }
+  return res.status(400).send({message:'Nincs ilyen lépésadat!'});
+});
+
+// DELETE step by id
+app.delete('/steps/:id', (req, res)=>{
   let id = req.params.id;
   let idx = steps.findIndex(step => step.id == id);
-  if (idx > -1) {
-    steps.splice(idx, 1);
-    saveSteps()
-    return res.send({msg: 'Lépés törölve'});
-  }
-  return res.status(400).send({msg: 'Nincs ilyen azonosítójű lépés'})
-})
-// DELETE all steps by userID
-app.delete("/steps/user/:userID",(req,res)=>{
-  let id = req.params.userID;
 
-  let data = req.params
-  let arrayCount = steps.length;
-  let idx = steps.findIndex(step => step.userID == id);
-  if (idx > -1) {
-    for (let i = 0; i < arrayCount; i++) {
-      if(steps[i].userID == data.userID){
-        steps.splice(idx, 1);
-        saveSteps()
-      }
-    }
-    return res.send({msg: 'Lépések törölve a felhasználótól'});
+  if (idx == -1){
+      res.status(400).send({message: 'Nincs ilyen lépésadat!'});
+      return
   }
-  return res.status(400).send({msg: 'Nincs ilyen azonosítójű lépés'})
-})
 
-app.delete("/steps", (_req,res)=>{
-  steps = []
-  saveSteps()
-  res.send({message: "Sikeresen törölve az összes lépés!"})
-})
+  steps.splice(idx, 1);
+  saveSteps();
+  res.send({message: 'Lépésadat sikeresen törölve!'});
+});
+
+// DELETE all steps by userId
+app.delete('/steps/user/:uid', (req, res)=>{
+  let userId = req.params.uid;
+  let idx = users.findIndex(user => user.id == userId);
+
+  if (idx == -1){
+      res.status(400).send({message: 'Nincs ilyen felhasználó!'});
+      return
+  }
+  
+  steps = steps.filter( step => step.userId != userId);
+  saveSteps();
+  res.send({message: 'Lépésadatok sikeresen törölve!'});
+});
+
+// DELETE all steps of users
+app.delete('/steps', (req, res)=>{
+  steps = [];
+  saveSteps();
+  res.send({message: 'Az összes lépésadat sikeresen törölve!'});
+}); 
 
 app.listen(3000);
 
